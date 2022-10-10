d206d80f04f42c8795fe556c15116756b7f9d857
Nitin <nitinkm320@gmail.com>
2022-10-10 16:38:12 +0530

    Made number selector

--- solver.js
+++ solver.js
@@ -15,12 +15,31 @@ var squareSelected;
 var canSelect;
 
 window.onload = function () {
+
     funcId("startsolver").addEventListener("click", start);
+
+    for(let i=0; i<9; i++){
+        funcId("numbers").children[i].addEventListener("click", function () {
+            
+                if(this.classList.contains("selected")) {
+                    this.classList.remove("selected");
+                }
+                else {
+                    for(let i=0; i<9; i++) {
+                        funcId("numbers").children[i].classList.remove("selected");
+                    }
+                    this.classList.add("selected")
+                }
+            
+        });
+    }
 }
+
 
 function start () {
     canSelect = false;
     startsolver.style.display = 'none';
+    body.style.overflowY = 'auto';
     makeBoard();
 }
 
@@ -48,6 +67,57 @@ function makeBoard () {
         funcId("board").appendChild(square);
         
     }
+
+    let one = document.createElement("p");
+    one.textContent = '1';
+    one.classList.add("one");
+    funcId("numbers").appendChild(one);
+
+    let two = document.createElement("p");
+    two.textContent = '2';
+    two.classList.add("two");
+    funcId("numbers").appendChild(two);
+
+    let three = document.createElement("p");
+    three.textContent = '3';
+    three.classList.add("three");
+    funcId("numbers").appendChild(three);
+
+    let four = document.createElement("p");
+    four.textContent = '4';
+    four.classList.add("four");
+    funcId("numbers").appendChild(four);
+
+    let five = document.createElement("p");
+    five.textContent = '5';
+    five.classList.add("five");
+    funcId("numbers").appendChild(five);
+
+    let six = document.createElement("p");
+    six.textContent = '6';
+    six.classList.add("six");
+    funcId("numbers").appendChild(six);
+
+    let seven = document.createElement("p");
+    seven.textContent = '7';
+    seven.classList.add("seven");
+    funcId("numbers").appendChild(seven);
+
+    let eight = document.createElement("p");
+    eight.textContent = '8';
+    eight.classList.add("eight");
+    funcId("numbers").appendChild(eight);
+
+    let nine = document.createElement("p");
+    nine.textContent = '9';
+    nine.classList.add("nine");
+    funcId("numbers").appendChild(nine);
+
+    let ten = document.createElement("p");
+    ten.textContent = '10';
+    ten.classList.add("ten");
+    funcId("numbers").appendChild(ten);
+    
 }
 
 function clearOldBoard () {
