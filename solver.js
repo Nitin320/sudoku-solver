7811d8981bbd798e0834e3e945e9f9b331501696
Nitin <nitinkm320@gmail.com>
2022-10-15 00:03:30 +0530

    Updated solve speed

--- solver.js
+++ solver.js
@@ -180,7 +180,7 @@ function funcId(id) {
 var finalInd;
 
 function sleep1() {
-    return new Promise(resolve => setTimeout(resolve, 50));
+    return new Promise(resolve => setTimeout(resolve, 25));
 }
 
 async function solve1 () {
